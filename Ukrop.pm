35d42a3959043e10a8b1243393928db44536bb29
Dmitry Karasik <dmitry@karasik.eu.org>
2008-04-10 13:10:02 +0000

    Import of KARASIK/Acme-Ukrop-0.01 from CPAN.
    gitpan-cpan-distribution: Acme-Ukrop
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         KARASIK/Acme-Ukrop-0.01.tar.gz
    gitpan-cpan-author:       KARASIK
    gitpan-cpan-maturity:     released

---
+++ Ukrop.pm
@@ -0,0 +1,73 @@
+#  Content-Encoding: koi8-u
+#  
+#  $Id: Ukrop.pm,v 1.1 2008/04/10 13:07:17 dk Exp $
+
+use strict;
+
+package Acme::Ukrop;
+
+use vars qw($VERSION);
+
+$VERSION = '0.01';
+
+my %n = (
+	'взад'     => 'return',
+	'або'      => 'else',
+	'то'       => '{',
+	'отож'     => '}',
+	'так'      => '',
+	'нехай'    => 'my',
+	'кажи'     => 'print',
+	'дiйство'  => 'sub',
+	'доки'     => 'while',
+	'якщо'     => 'if',
+	'довжина'  => 'length',
+	'геть'     => 'break',
+	'вiдрiжемочи⌡що'=> 'chomp',
+);
+
+my $k  = join('|', sort keys %n);
+my $nc = qr/[^a-zA-Z\x80-\xff]/;
+$k = qr/(^|$nc)($k)(?=$|$nc)/;
+use Filter::Simple sub { s/$k/$1$n{$2}/gs } ;
+
+1;
+
+=pod
+
+=head1 NAME
+
+Acme::Ukrop - ukrop parser
+
+=head1 DISCAIMER
+
+This is a preliminary implementation of parser of language Ukrop, based on
+ukrainian language. Internally, the language is the same perl but with
+ukrainian keywords instead. If you don't know ukrainian, don't bother.
+
+=head1 DESCRIPTION
+
+Первый Настоящий Укропопарсер магически придает программам написанным на Укропе
+все возможности перла.
+
+=head1 SYNOPSIS
+
+	use Acme::Ukrop;
+	доки (<>) то
+		вiдрiжемочи⌡що;
+		кажи "ти казав: $_\n";
+	так отож
+
+=head1 SEE ALSO
+
+http://community.livejournal.com/ru_ukrop/
+
+=head1 THANKS
+
+Kiev.pm for help with inseminating the Ukrop.
+
+=head1 AUTHOR
+
+Dmitry Karasik, E<lt>dmitry@karasik.eu.orgE<gt>.
+
+=cut
